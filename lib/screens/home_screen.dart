bc1dcc4c6da105a650924aae8fb3ab98c4cd98e0
Tanvi Gupta <tanvigupta129@gmail.com>
2022-05-14 00:04:40 +0530

    🚀 Cat and Dog Model Successfully Integrated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'dart:math';
+import 'package:tflite/tflite.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 class HomePage extends StatefulWidget {
@@ -7,12 +8,20 @@ class HomePage extends StatefulWidget {
 
   @override
   State<HomePage> createState() => _HomePageState();
+
 }
 
 class _HomePageState extends State<HomePage> {
 
   XFile? imageurl;
   final ImagePicker _picker = ImagePicker();
+  bool isimageselected = false;
+  String _confidence = "Not Loaded";
+  String _name = "Not loaded";
+  String numbers = '';
+  List? _result;
+
+  
 
   Future getImage(bool isCamera)async{
     XFile? image = await _picker.pickImage(
@@ -21,8 +30,40 @@ class _HomePageState extends State<HomePage> {
 
     setState(() {
        imageurl = image;
+       isimageselected = true;
+       applyModelOnImage(imageurl);
     });
 
+  }
+  //to be called in initState
+
+  loadmyModel() async{
+    var resultant = await Tflite.loadModel(model: "assests/models/model_unquant.tflite",
+    labels: "assests/models/labels.txt");
+
+    print("Result after loading model: $resultant");
+  }
+  applyModelOnImage(XFile? file)async{
+    var res = await Tflite.runModelOnImage(path: file!.path,
+    numResults: 2,
+    threshold: 0.5,
+    imageMean: 127.5,
+    imageStd: 127.5);
+
+    setState(() {
+      _result = res;
+      String str = _result?[0]["label"];
+      _name = str.substring(2);
+      _confidence = _result!=null ? (_result?[0]['confidence']*100.0).toString().substring(0,2) + "%" : '';
+    });
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadmyModel();
   }
   @override
   Widget build(BuildContext context) {
@@ -32,8 +73,19 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: Container(
-        child: imageurl==null? const Text("No Image"):
-        Image.file(File(imageurl!.path)),),
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              child: isimageselected? 
+              Image.file(File(imageurl!.path)):  Container(child:  Text("No Image Selected")),),
+              Text("Name : $_name"),
+              Text("Confidence : $_confidence"),
+          ],
+        ),
+      ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
